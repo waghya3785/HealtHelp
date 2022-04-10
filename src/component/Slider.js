ffb368c706dc8d70c74e5f1a783052da201b8f34
PRAKALP-PANDE <91276322+PRAKALP-PANDE@users.noreply.github.com>
2022-04-11 01:47:44 +0530

    Slider version 1.1.2
    Updated carousel, added text on right side of picture

--- src/component/Slider.js
+++ src/component/Slider.js
@@ -2,47 +2,89 @@ import React from 'react'
 
 export default function Slider() {
     return (
-        <div className = "container my-4">
-            <div className="container my-4">
-                <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
+        <div>
+            <div className="container" style={{margin: '101px'}} >
+                <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
                     <div className="carousel-indicators">
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active"
+                            aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                            aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                            aria-label="Slide 3"></button>
                     </div>
                     <div className="carousel-inner">
                         <div className="carousel-item active">
-                            <img src="https://mbcnschool.files.wordpress.com/2014/01/school-for-physically-challenged.jpg?w=640" className="d-block w-100" alt="..." />
-                            <div className="carousel-caption d-none d-md-block">
-                                <h5>First slide label</h5>
-                                <p>Some representative placeholder content for the first slide.</p>
+
+                            <div className="row">
+                                <div className="container col-lg-6">
+                                    <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxhUubLP9MxqRn5WyTihFEIR4UWvLDN9L7lA&usqp=CAU"} className="d-block w-100" alt="..." />
+                                </div>
+                                <div className="container col-lg-6">
+                                    <div className="card">
+                                        <div className="card-body">
+                                            <h5 className="card-title">Card title</h5>
+                                            <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                                            <p className="card-text">Some quick example text to build on the card title and make up
+                                                the
+                                                bulk of the card's content.</p>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         <div className="carousel-item">
-                            <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxhUubLP9MxqRn5WyTihFEIR4UWvLDN9L7lA&usqp=CAU"} className="d-block w-100" alt="..." />
-                            <div className="carousel-caption d-none d-md-block">
-                                <h5>Second slide label</h5>
-                                <p>Some representative placeholder content for the second slide.</p>
+
+                            <div className="row">
+                                <div className="container col-lg-6">
+                                    <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxhUubLP9MxqRn5WyTihFEIR4UWvLDN9L7lA&usqp=CAU"} className="d-block w-100" alt="..." />
+                                </div>
+                                <div className="container col-lg-6">
+                                    <div className="card">
+                                        <div className="card-body">
+                                            <h5 className="card-title">Card title</h5>
+                                            <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                                            <p className="card-text">Some quick example text to build on the card title and make up
+                                                the
+                                                bulk of the card's content.</p>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         <div className="carousel-item">
-                            <img src="http://4.bp.blogspot.com/-y-JDXpmWg8Q/U_xezds7BHI/AAAAAAAAA84/HtUmuQM33uk/s1600/child%2Bat%2Bschools%2Bfor%2Bmentally%2Bchallenged.jpg" className="d-block w-100" alt="..." />
-                            <div className="carousel-caption d-none d-md-block">
-                                <h5>Third slide label</h5>
-                                <p>Some representative placeholder content for the third slide.</p>
+
+                            <div className="row">
+                                <div className="container col-lg-6">
+                                    <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxhUubLP9MxqRn5WyTihFEIR4UWvLDN9L7lA&usqp=CAU"} className="d-block w-100" alt="..." />
+                                </div>
+                                <div className="container col-lg-6">
+                                    <div className="card">
+                                        <div className="card-body">
+                                            <h5 className="card-title">Card title</h5>
+                                            <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                                            <p className="card-text">Some quick example text to build on the card title and make up
+                                                the
+                                                bulk of the card's content.</p>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+                        data-bs-slide="prev">
                         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                         <span className="visually-hidden">Previous</span>
                     </button>
-                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+                        data-bs-slide="next">
                         <span className="carousel-control-next-icon" aria-hidden="true"></span>
                         <span className="visually-hidden">Next</span>
                     </button>
                 </div>
             </div>
+
         </div>
     )
 }
