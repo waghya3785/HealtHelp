a55e2c592f6b3a65894b43c37b54fe06ef2eb4cf
PRAKALP-PANDE <91276322+PRAKALP-PANDE@users.noreply.github.com>
2022-04-11 01:48:53 +0530

    Fund version 1.1.2
    Added carousel in fund section and resolve some errors

--- src/component/Fund.js
+++ src/component/Fund.js
@@ -3,29 +3,123 @@ import React from 'react'
 export default function Fund() {
     return (
         <div>
-            <div className="container mx-4 my-4">
-                <h2 className="text-center">Fund raiser</h2>
-                <div className="card-group">
-                    <div className="card">
-                        <img src="..." className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        </div>
+            <div className="container my-4">
+            <hr/>
+                <h2 className="text-center">Fund Raiser</h2>
+                <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+                    <div className="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active"
+                            aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                            aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                            aria-label="Slide 3"></button>
                     </div>
-                    <div className="card">
-                        <img src="..." className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                    <div className="carousel-inner">
+                        <div className="carousel-item active">
+
+                            <div className="card-group">
+                                <div className="card mx-2 bg-secondary p-2 text-dark bg-opacity-10">
+                                <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxhUubLP9MxqRn5WyTihFEIR4UWvLDN9L7lA&usqp=CAU"} className="d-block w-100" alt="..." />                                    <div className="card-body">
+                                        <h5 className="card-title">Card title</h5>
+                                        <p className="card-text">This is a wider card with supporting text below as a natural
+                                            lead-in to additional content. This content is a little bit longer.</p>
+                                    </div>
+                                </div>
+                                <div className="card mx-2 bg-secondary p-2 text-dark bg-opacity-10">
+                                    <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxhUubLP9MxqRn5WyTihFEIR4UWvLDN9L7lA&usqp=CAU"} className="d-block w-100" alt="..." />
+                                    <div className="card-body">
+                                        <h5 className="card-title">Card title</h5>
+                                        <p className="card-text">This card has supporting text below as a natural lead-in to
+                                            additional content.</p>
+                                    </div>
+                                </div>
+                                <div className="card mx-2 bg-secondary p-2 text-dark bg-opacity-10">
+                                    <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxhUubLP9MxqRn5WyTihFEIR4UWvLDN9L7lA&usqp=CAU"} className="d-block w-100" alt="..." />
+                                    <div className="card-body">
+                                        <h5 className="card-title">Card title</h5>
+                                        <p className="card-text">This is a wider card with supporting text below as a natural
+                                            lead-in to additional content. This card has even longer content than the first to
+                                            show that equal height action.</p>
+                                    </div>
+                                </div>
+                            </div>
+
                         </div>
+                        <div className="carousel-item">
+
+                            <div className="card-group">
+                                <div className="card mx-2 bg-secondary p-2 text-dark bg-opacity-10">
+                                    <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxhUubLP9MxqRn5WyTihFEIR4UWvLDN9L7lA&usqp=CAU"} className="d-block w-100" alt="..." />
+                                    <div className="card-body">
+                                        <h5 className="card-title">Card title</h5>
+                                        <p className="card-text">This is a wider card with supporting text below as a natural
+                                            lead-in to additional content. This content is a little bit longer.</p>
+                                    </div>
+                                </div>
+                                <div className="card mx-2 bg-secondary p-2 text-dark bg-opacity-10">
+                                    <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxhUubLP9MxqRn5WyTihFEIR4UWvLDN9L7lA&usqp=CAU"} className="d-block w-100" alt="..." />
+                                    <div className="card-body">
+                                        <h5 className="card-title">Card title</h5>
+                                        <p className="card-text">This card has supporting text below as a natural lead-in to
+                                            additional content.</p>
+                                    </div>
+                                </div>
+                                <div className="card mx-2 bg-secondary p-2 text-dark bg-opacity-10">
+                                    <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxhUubLP9MxqRn5WyTihFEIR4UWvLDN9L7lA&usqp=CAU"} className="d-block w-100" alt="..." />
+                                    <div className="card-body">
+                                        <h5 className="card-title">Card title</h5>
+                                        <p className="card-text">This is a wider card with supporting text below as a natural
+                                            lead-in to additional content. This card has even longer content than the first to
+                                            show that equal height action.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="carousel-item">
+
+                            <div className="card-group">
+                                <div className="card mx-2 bg-secondary p-2 text-dark bg-opacity-10">
+                                    <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxhUubLP9MxqRn5WyTihFEIR4UWvLDN9L7lA&usqp=CAU"} className="d-block w-100" alt="..." />
+                                    <div className="card-body">
+                                        <h5 className="card-title">Card title</h5>
+                                        <p className="card-text">This is a wider card with supporting text below as a natural
+                                            lead-in to additional content. This content is a little bit longer.</p>
+                                    </div>
+                                </div>
+                                <div className="card mx-2 bg-secondary p-2 text-dark bg-opacity-10">
+                                    <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxhUubLP9MxqRn5WyTihFEIR4UWvLDN9L7lA&usqp=CAU"} className="d-block w-100" alt="..." />
+                                    <div className="card-body">
+                                        <h5 className="card-title">Card title</h5>
+                                        <p className="card-text">This card has supporting text below as a natural lead-in to
+                                            additional content.</p>
+                                    </div>
+                                </div>
+                                <div className="card mx-2 bg-secondary p-2 text-dark bg-opacity-10">
+                                    <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxhUubLP9MxqRn5WyTihFEIR4UWvLDN9L7lA&usqp=CAU"} className="d-block w-100" alt="..." />
+                                    <div className="card-body">
+                                        <h5 className="card-title">Card title</h5>
+                                        <p className="card-text">This is a wider card with supporting text below as a natural
+                                            lead-in to additional content. This card has even longer content than the first to
+                                            show that equal height action.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
-                    <div className="card">
-                        <img src="..." className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                        </div>
+                    <div className="container mx-4">
+
+                        <button className="carousel-control-prev" type="button"
+                            data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Previous</span>
+                        </button>
+                        <button className="carousel-control-next" type="button"
+                            data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Next</span>
+                        </button>
                     </div>
                 </div>
             </div>
