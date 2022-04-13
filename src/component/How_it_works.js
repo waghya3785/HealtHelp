fe4c6925f01795d6bb8baaee3f9232f86d26c40d
TANMAY WAGHOLE <102225933+waghya3785@users.noreply.github.com>
2022-04-13 11:17:29 +0530

    Create How_it_works.js

---
+++ src/component/How_it_works.js
@@ -0,0 +1,145 @@
+import React from 'react'
+// import Ani from './Ani'
+
+import './Style.css'
+ 
+
+
+
+
+export default function How_it_works() {
+  
+
+
+  return (
+    <div>
+      <div className="row" style={{margin : '160px 0px',justifyContent:"center"}}>
+      <div className="card my-4 col-md-4" style={{width:"18rem",justifyContent:"center",margin : '160px 0px'}}>
+  <img src="..." className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+  </div>
+</div>
+      
+  
+ <div className="img">
+   
+    
+  
+  <div style={{margin : '160px 0px'}}>
+        
+  
+
+   
+      
+     
+<div className="card ">
+  <div className="card-body text">
+  <h4 className="heading">What is Crowdfunding?</h4><br></br>
+In its simplest form, Crowdfunding is a practice of giving monetary funds to<br></br> overcome specific social, cultural, or economic hurdles individuals face in their<br></br> daily lives.<br></br>
+  </div>
+</div>
+    <div style={{margin : '60px 0px'}}>
+    <div  className="accordion" id="accordionExample">
+  <div  className="accordion-item">
+    <h2  className="accordion-header" id="headingOne">
+      <button  className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+      How do I raise funds?
+      </button>
+    </h2>
+    <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+      <div className="accordion-body">
+      To Start a Fundraiser, follow the three steps:<br></br>
+1.Open the "Fundraiser" option<br></br>
+2. Fill up the form<br></br>
+3. Hit Submit<br></br>
+      </div>
+    </div>
+  </div>
+  <div className="accordion-item">
+    <h2 className="accordion-header" id="headingTwo">
+      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+      Can I raise funds for a friend as well?
+      </button>
+    </h2>
+    <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+      <div className="accordion-body">
+      Yes, you can also raise funds for a friend, a loved one or anyone in need during life's crucial moments
+      </div>
+    </div>
+  </div>
+  <div className="accordion-item">
+    <h2 className="accordion-header" id="headingThree">
+      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+      Does the raised amount reach the individual directly?
+      </button>
+    </h2>
+    <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+      <div className="accordion-body">
+      With HealtHelp, the money you collect goes directly to the bank account associated with your fundraising page. Raising money for yourself or anyone has never been easier.
+      </div>
+    </div>
+  </div>
+  <div className="accordion-item">
+    <h2 className="accordion-header" id="headingFour">
+      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+      Is it safe?
+      </button>
+    </h2>
+    <div id="collapseFour" className="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
+      <div className="accordion-body">
+      Your HealtHelp fundraiser features the very best in secure payment encryption technology. Not only are your donors online payments safe, your money is stored securely until you're ready to request a withdrawal via electronic bank transfer.
+      </div>
+    </div>
+  </div>
+  <div className="accordion-item">
+    <h2 className="accordion-header" id="headingFive">
+      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+      What if I don't reach my goal?
+      </button>
+    </h2>
+    <div id="collapseFive" className="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
+      <div className="accordion-body">
+      No problem. You will still receive the raised amount after the transactional processing fee.
+To know more about transactional processing fee, visit: www.ketto.org/support/plans-and-pricing
+      </div>
+    </div>
+  </div>
+  <div className="accordion-item">
+    <h2 className="accordion-header" id="headingSix">
+      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+      I have more questions, who do I write to?
+      </button>
+    </h2>
+    <div id="collapseSix" className="accordion-collapse collapse" aria-labelledby="headingSix" data-bs-parent="#accordionExample">
+      <div className="accordion-body">
+      You can write to info@ketto.org Know someone who needs funds for medical treatment urgently? Help them out by sharing this information with them.
+      </div>
+    </div>
+  </div>
+ 
+ 
+</div>
+
+<div className="d-grid gap-2 d-md-flex justify-content-md-end my-3">
+  <button className="btn btn-primary me-md-2" type="button">Button</button>
+  
+
+</div>
+{/* <Button variant="contained" style={{display: 'flex', justifyContent: 'right'}} color="primary" className="float-right" handleOnClick={this.onSend}></Button> */}
+</div>
+</div>
+</div>
+</div>
+
+
+
+
+
+ 
+  )
+}
+
