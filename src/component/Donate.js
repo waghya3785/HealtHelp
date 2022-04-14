ebf674176c8642b8611e986cd3c09d423d3c3a71
AdityaPadwal2892 <103318635+AdityaPadwal2892@users.noreply.github.com>
2022-04-14 19:25:49 +0530

    Update Donate.js

--- src/component/Donate.js
+++ src/component/Donate.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import "./heading.css"
+
 
 import D1 from "./picture/D1.jpg"
 import D2 from "./picture/D2.jpg"
@@ -8,11 +10,14 @@ import D3 from "./picture/D3.jpg"
 export default function Donate() {
 
   return (
+    <div style={{backgroundColor:'#F2FFF9'}}>
 
-    <div className="container">
+    
+
+    <div className="container"style={{marginTop:'75px'}}>
 
 
-          <h1 className='card-title justify-content-center' style={{marginLeft:"500px"}}>Why to donate</h1>
+          <h1 className='card-title justify-content-center animate-character' style={{marginLeft:"500px"}}>Why to donate</h1>
           <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel" style={{ borderRadius: "15px", boxShadow: "2px 2px 6px", height: "75%", width: "75%", margin: "auto" }}>
       
         <div className="carousel-indicators">
@@ -21,23 +26,23 @@ export default function Donate() {
           <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
         </div>
         
-        <div className="carousel-inner">
+        <div className="carousel-inner" >
           <div className="carousel-item active" >
-            <img src={D1} className="d-block w-100" alt="..." style={{ borderRadius: "15px", boxShadow: "2px 2px 6px", height: "75%", width: "75%", margin: "auto" }} />
+            <img src={D1} className="d-block w-100" alt="..." style={{ borderRadius: "15px", boxShadow: "2px 2px 6px", height: "80%", width: "75%", margin: "auto" }} />
             <div className="carousel-caption d-none d-md-block">
               <h5>“I have found that among its other benefits, giving liberates the soul of the giver."</h5>
               <p>Maya Angelou</p>
             </div>
           </div>
-          <div className="carousel-item">
-            <img src={D2} className="d-block w-100" alt="..." style={{ borderRadius: "15px", boxShadow: "2px 2px 6px", height: "75%", width: "75%", margin: "auto" }} />
+          <div className="carousel-item" >
+            <img src={D2} className="d-block w-100" alt="..." style={{ borderRadius: "15px", boxShadow: "2px 2px 6px", height: "80%", width: "75%", margin: "auto" }} />
             <div className="carousel-caption d-none d-md-block">
               <h5>“Always give without remembering and always receive without forgetting."</h5>
               <p>Brian Tracy</p>
             </div>
           </div>
-          <div className="carousel-item">
-            <img src={D3} className="d-block w-100" alt="..." style={{ borderRadius: "15px", boxShadow: "2px 2px 6px", height: "75%", width: "75%", margin: "auto" }} />
+          <div className="carousel-item" >
+            <img src={D3} className="d-block w-100" alt="..." style={{ borderRadius: "15px", boxShadow: "2px 2px 6px", height: "80%", width: "75%", margin: "auto" }} />
             <div className="carousel-caption d-none d-md-block">
               <h5>“We make a living by what we get. We make a life by what we give."</h5>
               <p>Winston Churchill.</p>
@@ -79,7 +84,7 @@ export default function Donate() {
       <div className="row row-cols-1 row-cols-md-3 g-4 " style={{ margin: "85px 0px" }} >
         <div className="col d-flex justify-content-center">
           <div className="card" style={{ borderRadius: "15px", boxShadow: "2px 2px 6px" }}>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3lmVpFe5cwFoXLp_8D0hINrBLw0fjrWbjTQ&usqp=CAU" className="card-img-top" alt="..." style={{ borderRadius: "15px", height: "250px" }} />
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3lmVpFe5cwFoXLp_8D0hINrBLw0fjrWbjTQ&usqp=CAU" className="card-img-top" alt="..." style={{ borderRadius: "15px", height: "250px",width:'25.3rem' }} />
             <div className="card-body">
               <h4 className="card-title">For Need of food</h4>
               <p className="card-text"><p>21% of people live on less than US$1.90 a day
@@ -96,8 +101,8 @@ export default function Donate() {
 
         <div className="col">
           <div className="card" style={{ borderRadius: "15px", boxShadow: "2px 2px 6px" }}>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTbQtLL9NwNl8PZTV3WTs10WrutuQqXQCq9w&usqp=CAU" className="card-img-top" alt="..." style={{ borderRadius: "15px", height: "250px" }} />
-            <div className="card-body">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTbQtLL9NwNl8PZTV3WTs10WrutuQqXQCq9w&usqp=CAU" className="card-img-top" alt="..." style={{ borderRadius: "15px", height: "250px",width:'25.3rem' }} />
+            <div className="card-body"style={{height:'240px'}}>
               <h4 className="card-title">For Better Education</h4>
               <p className="card-text">
               <p>In India only 74.04% litrecy rate.
@@ -110,9 +115,9 @@ export default function Donate() {
         </div>
 
         <div className="col">
-          <div className="card" style={{ borderRadius: "15px", boxShadow: "2px 2px 6px" }}>
-            <img src="https://images.newindianexpress.com/uploads/user/imagelibrary/2020/8/24/w900X450/Bagalkot_village_school_EPS.jpg?w=400&dpr=2.6" className="card-img-top" alt="..." style={{ borderRadius: "15px", height: "250px" }} />
-            <div className="card-body">
+          <div className="card" style={{ borderRadius: "15px", boxShadow: "2px 2px 6px"  }}>
+            <img src="https://images.newindianexpress.com/uploads/user/imagelibrary/2020/8/24/w900X450/Bagalkot_village_school_EPS.jpg?w=400&dpr=2.6" className="card-img-top" alt="..." style={{ borderRadius: "15px", height: "250px",width:'25.3rem' }} />
+            <div className="card-body"style={{height:'240px'}}>
               <h4 className="card-title">For Better Facilities</h4>
               <p className="card-text">
               <p>Along with better education.
@@ -128,7 +133,7 @@ export default function Donate() {
       <div className="row row-cols-1 row-cols-md-3 g-4 justify-content-center">
         <div className="col">
           <div className="card mx-3" style={{ borderRadius: "15px", boxShadow: "2px 2px 6px" }}>
-            <img src="https://switchindia.org/images/work/pediatric-health-camp-neral02.jpg" className="card-img-top" alt="..."  style={{ borderRadius: "15px", height: "250px" }} />
+            <img src="https://switchindia.org/images/work/pediatric-health-camp-neral02.jpg" className="card-img-top" alt="..."  style={{ borderRadius: "15px", height: "250px",width:'23.8rem' }} />
             <div className="card-body">
               <h4 className="card-title">For Better Health</h4>
               <p className="card-text">
@@ -143,8 +148,8 @@ export default function Donate() {
 
         <div className="col">
           <div className="card" style={{ borderRadius: "15px", boxShadow: "2px 2px 6px" }}>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWA5nWCUkKy_4YwRv04lJ38gqZsg719n-7dw&usqp=CAU" className="card-img-top" alt="..." style={{ borderRadius: "15px", height: "250px" }} />
-            <div className="card-body">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWA5nWCUkKy_4YwRv04lJ38gqZsg719n-7dw&usqp=CAU" className="card-img-top" alt="..." style={{ borderRadius: "15px", height: "250px",width:'25.8rem' }} />
+            <div className="card-body" style={{height:'240px'}}>
               <h4 className="card-title">For social trainings</h4>
               <p className="card-text">
                 <p>Along with the health and education we need to improve social awarness in society.
@@ -166,7 +171,7 @@ export default function Donate() {
 
     </div>
     
-
+</div>
 
 
 
