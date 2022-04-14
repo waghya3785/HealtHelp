ee6d073a727e2d4c12843e7f4e361eef5fbacd2b
TANMAY WAGHOLE <102225933+waghya3785@users.noreply.github.com>
2022-04-14 15:22:23 +0530

    Update Footer.js

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,12 +1,13 @@
 import React from 'react'
-
+import logo from "./picture/loc.png"
 export default function Footer() {
     return (
-        <div>
+
+        <div style={{ backgroundColor: '#212529' }}>
             <div className="b-example-divider"></div>
 
             <div className="container">
-                <footer className="row row-cols-5 py-5 my-5 border-top">
+                <footer className="row row-cols-5 pt-5 mt-5 border-top">
                     <div className="col">
                         <a href="/" className="d-flex align-items-center mb-3 link-dark text-decoration-none">
                             <img src="https://i.ibb.co/bXdVW6d/logo-healthhelp.png" className="bi me-2" width="90x" height="32"></img>
@@ -21,33 +22,33 @@ export default function Footer() {
                     <div className="col">
                         <h5>Section</h5>
                         <ul className="nav flex-column">
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
+                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 ">Home</a></li>
+                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 ">Features</a></li>
+                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 ">Pricing</a></li>
+                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 ">FAQs</a></li>
+                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 ">About</a></li>
                         </ul>
                     </div>
 
                     <div className="col">
                         <h5>Section</h5>
                         <ul className="nav flex-column">
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
+                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 ">Home</a></li>
+                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 ">Features</a></li>
+                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 ">Pricing</a></li>
+                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 ">FAQs</a></li>
+                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 ">About</a></li>
                         </ul>
                     </div>
 
                     <div className="col">
                         <h5>Section</h5>
                         <ul className="nav flex-column">
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
+                            <li className="nav-item mb-2"><a href="https://goo.gl/maps/CM7wDAEWJqUJFaZu7" className="nav-link p-0 " target="_blank"><img src={logo}  style={{height:"30px",width:"38px"}}/>Locate Us</a></li>
+                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 ">Jeevan Vardhini Matimand Vidylay, Dive, Maharashtra, 412301</a></li>
+                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 ">Pricing</a></li>
+                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 ">FAQs</a></li>
+                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 ">About</a></li>
                         </ul>
                     </div>
                 </footer>
