f7430146317326d5d2918e4ea8f61985af7f6688
TANMAY WAGHOLE <102225933+waghya3785@users.noreply.github.com>
2022-04-14 15:25:59 +0530

    Update Navbar.js

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
@@ -9,6 +10,9 @@ export default function Navbar() {
           "https://i.ibb.co/bXdVW6d/logo-healthhelp.png"
           alt="HealtHelp" width="140" height="40" />
         </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
         <div className="container-fluid">
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
@@ -23,19 +27,22 @@ export default function Navbar() {
                   How it works
                 </a>
                 <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a className="dropdown-item" href="/">How it works</a></li>
+                <li><Link className="dropdown-item"  to="/How_it_works">How it works</Link></li>
                   <li><a className="dropdown-item" href="/">Feedback</a></li>
-                  <li><a className="dropdown-item" href="/">Team</a></li>
+                  <li><Link className="dropdown-item"  to="/Team">Team</Link></li>
                   <li><a className="dropdown-item" href="/">Get in touch</a></li>
                   <li><hr className="dropdown-divider" /></li>
                   <li><a className="dropdown-item" href="/">Why To Donate</a></li>
                 </ul>
               </li>
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">About us</a>
+                <a className="nav-link active"  aria-current="page" href="#About">About us</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">Fundraiser</a>
+                <Link className="nav-link active"  aria-current="page" to="/Gallery">Gallery</Link>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#CreateFr">Fundraiser</a>
               </li>
             </ul>
             {/* <form className="d-flex">
