0724e8f3ab8e362c39afc2be49f8133f166e5eaa
TANMAY WAGHOLE <102225933+waghya3785@users.noreply.github.com>
2022-04-14 15:19:33 +0530

    Update Create_fundraiser.js

--- src/component/Create_fundraiser.js
+++ src/component/Create_fundraiser.js
@@ -2,46 +2,46 @@ import React from 'react'
 
 export default function Create_fundraiser() {
     return (
-        <div>
+        <div id="CreateFr">
             <div className="container my-4 shadow-lg p-3 mb-5 bg-body rounded">
-                <hr />
-                <h2 className="text-center">Create your Fundraiser</h2>
-                <div class="container">
-                    <div class="row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
-                        <div class="col">
-                            <div class="p-3 border bg-light">Orphanage</div>
+                <hr  style={{marginTop:'20px'}}/>
+                <h2 className="text-center animate-character"  style={{marginTop:'35px'}}>Create your Fundraiser</h2>
+                <div className="container">
+                    <div className="row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
+                        <div className="col">
+                            <div className="p-3 border bg-light">Orphanage</div>
                         </div>
-                        <div class="col">
-                            <div class="p-3 border bg-light">Animals</div>
+                        <div className="col">
+                            <div className="p-3 border bg-light">Animals</div>
                         </div>
-                        <div class="col">
-                            <div class="p-3 border bg-light">Patient</div>
+                        <div className="col">
+                            <div className="p-3 border bg-light">Patient</div>
                         </div>
-                        <div class="col">
-                            <div class="p-3 border bg-light">Old Age</div>
+                        <div className="col">
+                            <div className="p-3 border bg-light">Old Age</div>
                         </div>
-                        <div class="col">
-                            <div class="p-3 border bg-light">Education</div>
+                        <div className="col">
+                            <div className="p-3 border bg-light">Education</div>
                         </div>
-                        <div class="col">
-                            <div class="p-3 border bg-light">PHysically Challanged</div>
+                        <div className="col">
+                            <div className="p-3 border bg-light">PHysically Challanged</div>
                         </div>
-                        <div class="col">
-                            <div class="p-3 border bg-light">Row column</div>
+                        <div className="col">
+                            <div className="p-3 border bg-light">Row column</div>
                         </div>
-                        <div class="col">
-                            <div class="p-3 border bg-light">Row column</div>
+                        <div className="col">
+                            <div className="p-3 border bg-light">Row column</div>
                         </div>
-                        <div class="col">
-                            <div class="p-3 border bg-light">Row column</div>
+                        <div className="col">
+                            <div className="p-3 border bg-light">Row column</div>
                         </div>
-                        <div class="col">
-                            <div class="p-3 border bg-light">Row column</div>
+                        <div className="col">
+                            <div className="p-3 border bg-light">Row column</div>
                         </div>
                     </div>
                     <div className="container my-3">
-                        <div class="d-grid gap-2 col-6 mx-auto">
-                            <button class="btn btn-dark" type="button">Create Fundraiser</button>
+                        <div className="d-grid gap-2 col-6 mx-auto">
+                            <button className="btn btn-dark" type="button">Create Fundraiser</button>
                         </div>
                     </div>
                 </div>
