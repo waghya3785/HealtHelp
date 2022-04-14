1d62e7da284c40bb9242abc412db34dae052b216
TANMAY WAGHOLE <102225933+waghya3785@users.noreply.github.com>
2022-04-14 20:15:08 +0530

    Update Orphanage_slider.js

--- src/component/Orphanage_slider.js
+++ src/component/Orphanage_slider.js
@@ -1,37 +1,44 @@
 import React from 'react'
+import abt1 from "./picture/abt1.jpg"
+import abt2 from "./picture/abt2.jpg"
+import abt3 from "./picture/abt3.jpg"
+import abt4 from "./picture/abt4.jpg"
+import abt6 from "./picture/abt5.jpg"
+import abt5 from "./picture/abt6.jpg"
+import abt7 from "./picture/abt7.jpg"
+
+
 
 export default function Orphanage_slider() {
     return (
-        <div >
-            <div className="container" >
-                <h2 className="text-center " style={{marginTop:"80px" ,marginBottom:"50px"}} >About organization</h2>
-                <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
-                    <div className="carousel-indicators">
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                    </div>
-                    <div className="carousel-inner">
-                        <div className="carousel-item active">
-                            <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxhUubLP9MxqRn5WyTihFEIR4UWvLDN9L7lA&usqp=CAU"} className="d-block w-100" alt="..." />
-                        </div>
-                        <div className="carousel-item">
-                            <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxhUubLP9MxqRn5WyTihFEIR4UWvLDN9L7lA&usqp=CAU"} className="d-block w-100" alt="..." />
-                        </div>
-                        <div className="carousel-item">
-                            <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxhUubLP9MxqRn5WyTihFEIR4UWvLDN9L7lA&usqp=CAU"} className="d-block w-100" alt="..." />
-                        </div>
-                    </div>
-                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span className="visually-hidden">Previous</span>
-                    </button>
-                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span className="visually-hidden">Next</span>
-                    </button>
-                </div>
+        <div style={{marginTop:'100px',marginBottom:'725px'}}>
+            <img src={abt1} class="rounded float-start" alt="image1" style={{height:'200px', width:'200px', position:'relative', bottom:'80px', left:'15px'}}/>
+            <p style={{width:'900px',textAlign:'left',marginLeft:'350px'}}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere, nostrum deleniti quos sit, dolore doloribus quae porro veritatis voluptate id laborum quod ex, nulla rerum impedit temporibus consequatur incidunt minima?</p>
+            <img src={abt2} class="rounded float-end" alt="image2" style={{height:'200px', width:'200px', position:'relative', top:'142px',right:'25px'}}/>
+            <img src={abt3} class="rounded float-start" alt="image3" style={{height:'200px', width:'200px', position:'relative',top:'400px', right:'185px'}}/>
+
+            {/* <div>
+                <div>
+            <img src={abt1} class="rounded float-start" alt="img 1" style={{height:'200px', width:'200px', marginLeft:'27px', marginRight:'50px'}}/>
             </div>
+            <p style={{margin:'50px'}}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, fuga! Odit eos, eligendi dolores reprehenderit totam nihil voluptatem officiis error neque tenetur soluta necessitatibus quis repudiandae odio! Libero expedita veritatis error tempora nam, rem impedit alias mollitia est earum quam cumque eveniet corrupti cum cupiditate.</p>
+            </div>
+            
+            
+            <div>
+            <p style={{margin:'50px'}}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, fuga! Odit eos, eligendi dolores reprehenderit totam nihil voluptatem officiis error neque tenetur soluta necessitatibus quis repudiandae odio! Libero expedita veritatis error tempora nam, rem impedit alias mollitia est earum quam cumque eveniet corrupti cum cupiditate.</p>
+             <div>
+            <img src={abt2} class="rounded float-end" alt="img 1" style={{height:'200px', width:'200px', marginLeft:'27px', marginRight:'50px'}}/>
+            </div>
+            </div>
+            
+            
+            <>
+            
+        <img src={abt3} class="rounded float-start" alt="img 1" style={{height:'200px', width:'200px', marginLeft:'27px', marginRight:'50px'}}/>
+        </
+        <p style={{margin:'50px'}}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, fuga! Odit eos, eligendi dolores reprehenderit totam nihil voluptatem officiis error neque tenetur soluta necessitatibus quis repudiandae odio! Libero expedita veritatis error tempora nam, rem impedit alias mollitia est earum quam cumque eveniet corrupti cum cupiditate.</p>
+        </div> */}
         </div>
     )
 }
