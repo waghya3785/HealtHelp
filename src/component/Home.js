ea785a034da7c10e6b40369baa64a006f8e00d8e
TANMAY WAGHOLE <102225933+waghya3785@users.noreply.github.com>
2022-04-14 20:13:57 +0530

    Update Home.js

--- src/component/Home.js
+++ src/component/Home.js
@@ -5,21 +5,20 @@ import Fund from './Fund'
 import Orphanage_slider from './Orphanage_slider'
 import Detail_intro from './Detail_intro'
 import Result from './Result'
-
-
 import Create_fundraiser from './Create_fundraiser'
+import Gallery from './Gallery'
 export default function Home(props) {
   // props.setProgress(100);
   return (
-    <div style={{backgroundColor:"#F2FFF9"}}>
-     <Slider style={{border:"3px solid red v"}}/> 
-     <Cards/>
-     <Fund/>
-      <Orphanage_slider/>
-      <Detail_intro/>
-      <Result/>
-      <Create_fundraiser/>
-      
+    <div style={{ backgroundColor: "#F2FFF9" }}>
+      <Slider style={{ border: "3px solid red v" }} />
+      <Cards />
+      <Fund />
+      <Orphanage_slider />
+      <Detail_intro />
+      <Result />
+      <Create_fundraiser />
+      <Gallery />
     </div>
   )
 }
