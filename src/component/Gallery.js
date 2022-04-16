ac15e96c816b97a13391d0183704847307aa3193
Chillpeace <103319691+Chillpeace@users.noreply.github.com>
2022-04-16 17:59:42 +0530

    Add files via upload

--- src/component/Gallery.js
+++ src/component/Gallery.js
@@ -1,11 +1,57 @@
-import React from 'react'
+import React from "react";
+import "./Gallery.css";
+import orph1 from "./picture/orph1.jpeg";
+// import orph2 from "./picture/orph1.jpeg";
+// import orph3 from "./picture/orph1.jpeg";
+// import orph4 from "./picture/orph1.jpeg";
 
-export default function Gallery(props) {
-    props.setProgress(100);
+export default function Gallery() {
+  function myFunction(imgs) {
+    // Get the expanded image
+    var expandImg = document.getElementById("expandedImg");
+    // Get the image text
+    var imgText = document.getElementById("imgtext");
+    // Use the same src in the expanded image as the image being clicked on from the grid
+    expandImg.src = imgs.src;
+    // Use the value of the alt attribute of the clickable image as text inside the expanded image
+    imgText.innerHTML = imgs.alt;
+    // Show the container element (hidden with CSS)
+    expandImg.parentElement.style.display = "block";
+  }
+
   return (
     <div>
-        photos here
-      
+      <div className="row">
+        <div className="column">
+          <img src={orph1} alt="Nature" onClick={myFunction(this)} />
+        </div>
+        <div className="column">
+          <img src={orph1} alt="Snow" onClick={myFunction(this)} />
+        </div>
+        <div className="column">
+          <img
+            src={orph1}
+            alt="Mountains"
+            onClick={myFunction(this)}
+          />
+        </div>
+        <div className="column">
+          <img src={orph1} alt="Lights" onClick={myFunction(this)}/>
+        </div>
+      </div>
+
+      <div className="container">
+        <span
+          onClick="this.parentElement.style.display='none'"
+          className="closebtn"
+        >
+          &times;
+        </span>
+
+        <img id="expandedImg" style="width:100%" />
+
+        <div id="imgtext"></div>
+      </div>
     </div>
-  )
+  );
 }
